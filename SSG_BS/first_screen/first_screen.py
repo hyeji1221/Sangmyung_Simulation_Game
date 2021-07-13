9b401dbc02d18bd80d249a65a67498ce1ef9b181
bottle_sunny <61080784+An-Byeong-Seon@users.noreply.github.com>
2021-07-13 13:22:24 +0900

    Add files via upload

---
+++ SSG_BS/first_screen/first_screen.py
@@ -0,0 +1,34 @@
+import sys
+
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+#from basic_screen import basic_screen
+
+
+
+form_class=uic.loadUiType("first_screen.ui")[0]
+
+class WindowClass(QMainWindow, form_class):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.init.clicked.connect(self.initButton)
+        self.continue_2.clicked.connect(self.continueButton)
+        self.exit.clicked.connect(QCoreApplication.instance().quit)
+
+    def initButton(self):
+        print("init")
+    def continueButton(self):
+        print("continue")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow=WindowClass()
+    myWindow.show()
+    app.exec_()
