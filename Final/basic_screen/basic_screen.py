1c588afd7259a1bb4de11b2050b4ba4bba455d72
병선 <anbs0725@gmail.com>
2021-07-16 16:43:13 +0900

    .

--- Final/basic_screen/basic_screen.py
+++ Final/basic_screen/basic_screen.py
@@ -7,40 +7,60 @@ from PyQt5 import uic
 form_class=uic.loadUiType("basic_screen.ui")[0]
 
 class WindowClass(QMainWindow, form_class):
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.chat.setStyleSheet('QLabel {background-color: rgb(204,255,255)}')
-        self.menu.setStyleSheet('background: rgb(255,204,0)')
-        self.menu_screen.hide()
-        self.menu.clicked.connect(self.button_clicked)
+        self.menuBox.hide()
+
+        self.menu.setCheckable(True)
+        self.menu.clicked.connect(self.slot_toggle)
+
+    def slot_toggle(self):
+        if self.menu.isChecked():
+            self.button_clicked()
+        else:
+            self.menuOff()
+            self.menuBox.hide()
+
 
     def button_clicked(self):
-        self.speak.hide()
-        self.menu_screen.show()
-        self.quit.clicked.connect(QCoreApplication.instance().quit)
-        self.save.clicked.connect(self.saveButton)
-        self.mission.clicked.connect(self.missionButton)
+        self.menuBox.show()
         self.map.clicked.connect(self.mapButton)
-        self.out.clicked.connect(self.outButton)
+        self.mission.clicked.connect(self.missionButton)
+        self.time.clicked.connect(self.timeButton)
+        self.save.clicked.connect(self.saveButton)
+        self.exit.clicked.connect(self.close)
+
+    def mapButton(self):
+        print("map")
+
+    def missionButton(self):
+        print("mission")
 
     def saveButton(self):
         print("save")
-    def missionButton(self):
-        print("mission")
-    def mapButton(self):
-        print("map")
-    def outButton(self):
-        self.menu_screen.hide()
-        #self.quit.disconnect()
-        #self.save.disconnect()
-        #self.mission.disconnect()
-        #self.map.disconnect()
-        self.speak.show()
+
+    def timeButton(self):
+        print("time")
+
+    def menuOff(self):
+        self.map.disconnect()
+        self.mission.disconnect()
+        self.save.disconnect()
+        self.exit.disconnect()
+
+    def closeEvent(self, QCloseEvent):
+        re = QMessageBox.question(self, "종료 확인", "종료 하시겠습니까?", QMessageBox.Yes | QMessageBox.No)
+
+        if re == QMessageBox.Yes:
+            print("exit")
+            QCloseEvent.accept()
+        else:
+            print("Not exit")
+            QCloseEvent.ignore()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow=WindowClass()
+    myWindow = WindowClass()
     myWindow.show()
     app.exec_()
--- Final/first_screen/first_screen.py
+++ Final/first_screen/first_screen.py
@@ -3,12 +3,6 @@ import sys
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-import os
-
-sys.path.append("C:/Users/USER/OneDrive/바탕 화면/sangmyung_simulation_game-main/sangmyung_simulation_game-main/SSG_BS")
-#from . import basic_screen
-
-
 
 form_class=uic.loadUiType("first_screen.ui")[0]
 
