ac484ad979d2ea66f4467afbf3ba7f78861d8753
lg <thdgml5539@naver.com>
2021-07-27 20:25:45 +0900

    .

---
+++ SSG_sh00/basic_screen/basic_screen.py
@@ -0,0 +1,66 @@
+import sys
+
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class=uic.loadUiType("basic_screen.ui")[0]
+
+class WindowClass(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.menuBox.hide()
+
+        self.menu.setCheckable(True)
+        self.menu.clicked.connect(self.slot_toggle)
+
+    def slot_toggle(self):
+        if self.menu.isChecked():
+            self.button_clicked()
+        else:
+            self.menuOff()
+            self.menuBox.hide()
+
+
+    def button_clicked(self):
+        self.menuBox.show()
+        self.map.clicked.connect(self.mapButton)
+        self.mission.clicked.connect(self.missionButton)
+        self.time.clicked.connect(self.timeButton)
+        self.save.clicked.connect(self.saveButton)
+        self.exit.clicked.connect(self.close)
+
+    def mapButton(self):
+        print("map")
+
+    def missionButton(self):
+        print("mission")
+
+    def saveButton(self):
+        print("save")
+
+    def timeButton(self):
+        print("time")
+
+    def menuOff(self):
+        self.map.disconnect()
+        self.mission.disconnect()
+        self.save.disconnect()
+        self.exit.disconnect()
+
+    def closeEvent(self, QCloseEvent):
+        re = QMessageBox.question(self, "종료 확인", "종료 하시겠습니까?", QMessageBox.Yes | QMessageBox.No)
+
+        if re == QMessageBox.Yes:
+            print("exit")
+            QCloseEvent.accept()
+        else:
+            print("Not exit")
+            QCloseEvent.ignore()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
